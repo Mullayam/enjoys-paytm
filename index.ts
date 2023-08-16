74716e34d951ca16f95b7569feee51b29d973f16
Mulayam S <mullayam06@gmail.com>
2023-08-17 01:17:44 +0530

    export interface for ts support

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@
 /// <reference types="./src/types.d.ts" />
 import { PaytmConfigurationValidator } from "./src/interface";
 import { ServiceProvider } from "./src/lib";
+
+export  * from  "./src/interface"
 export class PaytmConfig extends ServiceProvider.Paytm {
   constructor(config: PaytmConfigurationValidator) {
     super(config);
